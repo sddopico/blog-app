2393598274fbd18583245887a2d150627535712a
Dominic Dopico <serafin.dopico@gmail.com>
2021-08-19 12:36:35 -0700

    Post model part 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,7 @@ exports.mustBeLoggedIn = function(req, res, next) {
 exports.login = function(req, res) {
     let user = new User(req.body)
     user.login().then(function(result) {
-        req.session.user = {avatar: user.avatar, username: user.data.username}
+        req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id}
         req.session.save(() => res.redirect('/'))
     }).catch(function(err) {
         req.flash('errors', err)
@@ -29,7 +29,7 @@ exports.logout = function(req, res) {
 exports.register = function(req, res) {
     let user = new User(req.body)
     user.register().then(() => {
-        req.session.user = {username: user.data.username, avatar: user.avatar}
+        req.session.user = {username: user.data.username, avatar: user.avatar, _id: user.data._id}
         req.session.save(() => res.redirect('/'))
     }).catch((regErrors) => {
         regErrors.forEach((err) => {
