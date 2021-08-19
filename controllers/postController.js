7ac107599378d61aca50a6d72feab295f2dfeb7c
Dominic Dopico <serafin.dopico@gmail.com>
2021-08-19 12:28:41 -0700

    Post model

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,14 @@
+const Post = require('../models/Post')
+
 exports.viewCreateScreen = function(req, res) {
     res.render('create-post')
+}
+
+exports.create = function(req, res) {
+    let post = new Post(req.body)
+    post.create().then(function() {
+        res.send('new post created')
+    }).catch(function(err) {
+        res.send(err)
+    })
 }
