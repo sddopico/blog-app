1b51e939fb4fe78c8610fe4dfce6eef948d676a7
Dominic Dopico <serafin.dopico@gmail.com>
2021-08-18 19:45:34 -0700

    user avatar

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const usersCollection = require('../db').db().collection('users')
 const validator = require('validator')
+const md5 = require('md5')
 
 let User = function(data) {
     this.data = data
@@ -77,6 +78,8 @@ User.prototype.login = function() {
         this.cleanUp()
         usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
             if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+                this.data = attemptedUser
+                this.getAvatar()
                 resolve('Congrats!')
             } else {
                 reject('Invalid username / password.')
@@ -98,12 +101,17 @@ User.prototype.register = function() {
             // Hash user password
             let salt = bcrypt.genSaltSync(10)
             this.data.password = bcrypt.hashSync(this.data.password, salt)
-            usersCollection.insertOne(this.data)
-           await resolve()
+            await usersCollection.insertOne(this.data)
+            this.getAvatar()
+            resolve()
         } else {
             reject(this.errors)
         }
     })
+}
+
+User.prototype.getAvatar = function() {
+    this.avatar = `https://gravatar.com/avatar/${md5('this.data.email')}?s=128`
 }
 
 module.exports = User
