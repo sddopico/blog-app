39410245113e9a47fe3a2b4ec78f3b5a8d100f4e
Dominic Dopico <serafin.dopico@gmail.com>
2021-08-19 19:39:14 -0700

    edit-post route guard

--- app.js
+++ app.js
@@ -5,27 +5,35 @@ const flash = require('connect-flash')
 const app = express()
 
 let sessionOptions = session({
-    secret: "Javascript is soooooooo cool",
-    store: new MongoStore({client: require('./db')}),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24,
-        httpOnly: true
-    }
+  secret: 'Javascript is soooooooo cool',
+  store: new MongoStore({ client: require('./db') }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  },
 })
 
 app.use(sessionOptions)
 app.use(flash())
 
-app.use(function(req, res, next) {
-    res.locals.user = req.session.user
-    next()
+app.use(function (req, res, next) {
+  //make current user id available on req object
+  if (req.session.user) {
+    req.visitorId = req.session.user._id
+  } else {
+    req.visitorId = 0
+  }
+
+  // make user session data available in view templates
+  res.locals.user = req.session.user
+  next()
 })
 
 const router = require('./router')
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use(express.static('public'))
